be3d04410a140651e1b423059cdef579bfba027a
Michael Irvine <michael.j.irvine@gmail.com>
2022-05-07 16:15:04 -0400

    data table works

--- components/QuestionResults/index.tsx
+++ components/QuestionResults/index.tsx
@@ -1,7 +1,19 @@
+import DataTable from "react-data-table-component";
+
 const QuestionResults = ({
   data,
 }: {
-  data: Record<string, string> | undefined;
-}) => <div>{JSON.stringify(data)}</div>;
+  data: Record<string, Array<Record<string, string | number>>> | undefined;
+}) => {
+  if (!data) return null;
+  const dataToDisplay = data["orders"];
+  console.info(JSON.stringify(dataToDisplay));
+  const columns = Object.keys(dataToDisplay[0]).map((key) => ({
+    name: key,
+    selector: (row: Record<string, string | number>) => row[key],
+    sortable: true,
+  }));
+  return <DataTable columns={columns} data={dataToDisplay} />;
+};
 
 export default QuestionResults;
