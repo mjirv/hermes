990fc4dd3b591b7784854a852005281775e3a958
Michael Irvine <michael.j.irvine@gmail.com>
2022-05-11 19:37:28 -0400

    hermes api works

--- services/NLPService.ts
+++ services/NLPService.ts
@@ -67,12 +67,18 @@ class OpenAIService implements NLPService {
 
 class HermesApiService implements NLPService {
   private url: string;
+  private clientId: string;
   private apiKey: string;
-  constructor(url: string | undefined, apiKey: string | undefined) {
-    if (!url || !apiKey) {
+  constructor(
+    url: string | undefined,
+    clientId: string | undefined,
+    apiKey: string | undefined
+  ) {
+    if (!url || !clientId || !apiKey) {
       throw new Error("must provide HERMES_API_URL and HERMES_API_KEY");
     }
     this.url = url;
+    this.clientId = clientId;
     this.apiKey = apiKey;
   }
   async getGraphQLQueryFromText({
@@ -82,15 +88,19 @@ class HermesApiService implements NLPService {
     query: string;
     graphQLSchema?: string | undefined;
   }): Promise<string> {
+    console.debug(`Getting graphQL query`);
     const res = await fetch(this.url, {
+      method: "POST",
       headers: {
-        authorization: `Bearer ${this.apiKey}`,
+        "X-CLIENT-ID": this.clientId,
+        "X-API-KEY": this.apiKey,
         "Content-Type": "application/json",
         accept: "application/json",
       },
       body: JSON.stringify({ query, graphQLSchema }),
     });
     const { graphQLQuery } = await res.json();
+    console.debug(`GraphQL query finished`, { graphQLQuery });
     return graphQLQuery;
   }
 }
@@ -98,6 +108,10 @@ class HermesApiService implements NLPService {
 const instance = process.env.OPENAI_API_KEY
   ? new OpenAIService()
   : process.env.HERMES_API_KEY
-  ? new HermesApiService(process.env.HERMES_API_URL, process.env.HERMES_API_KEY)
+  ? new HermesApiService(
+      process.env.HERMES_API_URL,
+      process.env.HERMES_API_CLIENT_ID,
+      process.env.HERMES_API_KEY
+    )
   : new MockNLPService();
 export default instance;
