a240a3b9dacc03c70cd15e6df922f8ed2a19b65a
Michael Irvine <michael.j.irvine@gmail.com>
2022-05-11 20:38:08 -0400

    updates services

--- services/NLPService.ts
+++ services/NLPService.ts
@@ -18,53 +18,6 @@ class MockNLPService implements NLPService {
   }
 }
 
-class OpenAIService implements NLPService {
-  private client: OpenAIApi;
-
-  constructor() {
-    if (!process.env.OPENAI_API_KEY) {
-      throw new Error("No OpenAI API key provided");
-    }
-    const configuration = new Configuration({
-      apiKey: process.env.OPENAI_API_KEY,
-    });
-    this.client = new OpenAIApi(configuration);
-  }
-
-  async getGraphQLQueryFromText({
-    query,
-    graphQLSchema,
-  }: {
-    query: string;
-    graphQLSchema: string;
-  }): Promise<string> {
-    console.debug(`Getting graphQL query`);
-    const response = await this.client.createCompletion("code-davinci-002", {
-      prompt:
-        "const schema = gql`" +
-        graphQLSchema +
-        "`\n\n/* don't fill in start_date and end_date */\n/* generates a query using `schema` to tell me: " +
-        query +
-        " */\nconst generateQuery = gql`\n    ",
-      suffix: "\n  `",
-      temperature: 0,
-      max_tokens: 256,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    });
-    if (!response.data.choices) {
-      throw new Error("Invalid request; no choices received from OpenAI");
-    }
-    const [{ text: graphQLQuery }] = response.data.choices;
-    console.debug(`GraphQL query finished`, { graphQLQuery });
-    if (!graphQLQuery) {
-      throw new Error("Invalid request; no completion received from OpenAI");
-    }
-    return graphQLQuery;
-  }
-}
-
 class HermesApiService implements NLPService {
   private url: string;
   private clientId: string;
@@ -105,9 +58,7 @@ class HermesApiService implements NLPService {
   }
 }
 
-const instance = process.env.OPENAI_API_KEY
-  ? new OpenAIService()
-  : process.env.HERMES_API_KEY
+const instance = process.env.HERMES_API_KEY
   ? new HermesApiService(
       process.env.HERMES_API_URL,
       process.env.HERMES_API_CLIENT_ID,
