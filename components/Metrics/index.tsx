5e6bec4ab75491902e8fcd5962943d13870e3134
Michael Irvine <michael.j.irvine@gmail.com>
2022-06-04 16:08:23 -0400

    metrics catalog works

--- components/Metrics/index.tsx
+++ components/Metrics/index.tsx
@@ -12,14 +12,16 @@ const Metrics = () => {
   }, [schema]);
 
   return (
-    <div className={styles.sidebar}>
-      <text className={styles.description}>{"Metrics Catalog"}</text>
-      <div className={styles.code}>
-        {schema
-          ? schema
-              .replaceAll(/(type Query|enum Grain) \{[\s\S]*?\}\n\n/g, "")
-              .trim()
-          : "loading..."}
+    <div className={styles.main}>
+      <div className={styles.content}>
+        <text className={styles.description}>{"Metrics Catalog"}</text>
+        <div className={styles.code}>
+          {schema
+            ? schema
+                .replaceAll(/(type Query|enum Grain) \{[\s\S]*?\}\n\n/g, "")
+                .trim()
+            : "loading..."}
+        </div>
       </div>
     </div>
   );
--- pages/queries/[encodedQuery].tsx
+++ pages/queries/[encodedQuery].tsx
@@ -1,32 +1,6 @@
 import type { GetServerSideProps } from "next";
-import Link from "next/link";
+import Header from "../../components/Header";
 import Query from "../../components/Query";
-import styles from "../../styles/Home.module.css";
-
-const Header = () => (
-  <div className={styles.header}>
-    <h1 className={styles.title}>
-      <Link href="/">Hermes</Link>
-    </h1>
-    <ul className={styles.headerLinks}>
-      <li>
-        <Link href="/">Home</Link>
-      </li>
-      <li>
-        <a
-          href="https://github.com/mjirv/Hermes"
-          target={"_blank"}
-          rel="noreferrer"
-        >
-          Docs
-        </a>
-      </li>
-      <li>
-        <Link href="/admin">Admin</Link>
-      </li>
-    </ul>
-  </div>
-);
 
 const QueryPage = ({ query }: { query: string }) => {
   return (
